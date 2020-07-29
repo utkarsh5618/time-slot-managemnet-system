cd353000af5e0a195ef5e757645f7335c15f4155
utkarsh5618 <46520828+utkarsh5618@users.noreply.github.com>
2020-07-29 12:07:34 +0530

    Add files via upload

---
+++ main.browser.ts
@@ -0,0 +1,33 @@
+/*
+ * Angular bootstraping
+ */
+import { platformBrowserDynamic }  from '@angular/platform-browser-dynamic';
+import { decorateModuleRef }       from './app/environment';
+import { bootloader }              from '@angularclass/hmr';
+import { getTranslationProviders } from './app/i18n-providers';
+/*
+ * App Module
+ * our top level module that holds all of our components
+ */
+import { AppModule }               from './app/app.module';
+
+/*
+ * Bootstrap our Angular app with a top level NgModule
+ */
+export function main(): Promise<any> {
+  // Get translation provider
+  return getTranslationProviders().then(
+    (providers) => {
+      const options = { providers };
+
+      return platformBrowserDynamic()
+        .bootstrapModule(AppModule, options)
+        .then(decorateModuleRef)
+        .catch((err) => console.error(err));
+    }
+  );
+}
+
+// needed for hmr
+// in prod this is replace for document ready
+bootloader(main);
